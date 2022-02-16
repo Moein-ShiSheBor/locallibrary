91bb98227ecc0c3c51a54bc2bf43b24aa4bac75b
Moein-ShiSheBor <shimoshebr@gmail.com>
2022-02-16 14:22:55 +0330

    com_2_base_site_up

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
+from . import views
 
-urlpatterns = []
+app_name = 'catalog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
